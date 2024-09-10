3bbc260a13241a70060be5bca76ae8aef8f723fb
kboiko2 <kboiko@broadleafcommerce.com>
2024-09-10 12:45:52 -0500

    Add service autoconfiguration as classpath landmark

---
+++ services/src/main/java/com/broadleafcommerce/subscriptionoperation/service/autoconfigure/SubscriptionOperationServiceAutoConfiguration.java
@@ -0,0 +1,7 @@
+package com.broadleafcommerce.subscriptionoperation.service.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SubscriptionOperationServiceAutoConfiguration {
+}
